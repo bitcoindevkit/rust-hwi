98ad3f7cb6717f5ddd61eeb09cecf8d0bc0e1c16
Daniela Brozzoni <brozzoni@live.it>
2020-03-05 16:54:25 +0100

    Add Device type, basic commands operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+#[macro_use] extern crate strum_macros;
+
+pub mod types;
+pub mod commands;
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    /*
+    #[test]
+    fn commands() {
+        use crate::commands::{HWICommand, Subcommand, Flag};
+        use std::process::Command;
+        use std::string::ToString;
+        let mut HWIcommand = HWICommand::new();
+        HWIcommand.add_subcommand(Subcommand::Enumerate);
+        HWIcommand.add_flag(Flag::Password(String::from("123")));
+        HWIcommand.add_flag(Flag::StdinPass);
+        println!("{:?}", HWIcommand.command);
+    }
+    */
 }
