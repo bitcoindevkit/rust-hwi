e65e516b4f73bd4be2b510a489e02a30cc58889c
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2020-03-07 17:57:06 +0100

    Add displayaddress, getdescriptors BIP43 account

--- src/interface.rs
+++ src/interface.rs
@@ -4,7 +4,7 @@ use bitcoin::util::bip32::DerivationPath;
 
 use crate::commands::{HWICommand, HWIFlag, HWISubcommand};
 use crate::error::Error;
-use crate::types::{HWIDescriptor, HWIDevice, HWIExtendedPubKey, HWISignature};
+use crate::types::{HWIAddress, HWIDescriptor, HWIDevice, HWIExtendedPubKey, HWISignature};
 
 pub fn enumerate() -> Result<Vec<HWIDevice>, Error> {
     let output = HWICommand::new()
@@ -21,6 +21,8 @@ pub fn get_master_xpub(device: &HWIDevice) -> Result<HWIExtendedPubKey, Error> {
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
+pub fn sign_tx(device: &HWIDevice) {}
+
 pub fn get_xpub(device: &HWIDevice, path: &DerivationPath) -> Result<HWIExtendedPubKey, Error> {
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
@@ -32,7 +34,7 @@ pub fn get_xpub(device: &HWIDevice, path: &DerivationPath) -> Result<HWIExtended
 
 pub fn sign_message(
     device: &HWIDevice,
-    message: &str,
+    message: &String,
     path: &DerivationPath,
 ) -> Result<HWISignature, Error> {
     let output = HWICommand::new()
@@ -44,10 +46,26 @@ pub fn sign_message(
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
-pub fn get_descriptors(device: &HWIDevice) -> Result<HWIDescriptor, Error> {
+pub fn get_keypool(device: &HWIDevice) {}
+
+pub fn get_descriptors(device: &HWIDevice, account: Option<u32>) -> Result<HWIDescriptor, Error> {
+    let account = match account {
+        Some(i) => i,
+        None => 0
+    };
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
         .add_subcommand(HWISubcommand::GetDescriptors)
+        .add_account(account)
+        .execute()?;
+    Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
+}
+
+pub fn display_address(device: &HWIDevice, descriptor: &String) -> Result<HWIAddress, Error> {
+    let output = HWICommand::new()
+        .add_flag(HWIFlag::Fingerprint(device.fingerprint))
+        .add_subcommand(HWISubcommand::DisplayAddress)
+        .add_descriptor(descriptor)
         .execute()?;
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,11 @@ mod tests {
     }
 
     fn get_first_device() -> HWIDevice {
-        interface::enumerate().unwrap().first().expect("No devices").clone()
+        interface::enumerate()
+            .unwrap()
+            .first()
+            .expect("No devices")
+            .clone()
     }
 
     #[test]
@@ -54,16 +58,33 @@ mod tests {
             ChildNumber::from_hardened_idx(44).unwrap(),
             ChildNumber::from_normal_idx(0).unwrap(),
         ]);
-        interface::sign_message(&device, "I love magical bitcoin wallet", &derivation_path)
-            .unwrap();
+        interface::sign_message(
+            &device,
+            &String::from("I love magical bitcoin wallet"),
+            &derivation_path,
+        )
+        .unwrap();
     }
 
     #[test]
     #[serial]
     fn test_get_descriptors() {
         let device = get_first_device();
-        let descriptor = interface::get_descriptors(&device).unwrap();
+        let account = Some(10);
+        let descriptor = interface::get_descriptors(&device, account).unwrap();
         assert!(descriptor.internal.len() > 0);
         assert!(descriptor.receive.len() > 0);
+    }
+
+    #[test]
+    #[serial]
+    fn test_display_address() {
+        let device = get_first_device();
+        let descriptor = interface::get_descriptors(&device, None).unwrap();
+        let descriptor = descriptor.receive.first().unwrap();
+        // Seems like hwi doesn't support descriptors checksums
+        let descriptor = &descriptor.split("#").collect::<Vec<_>>()[0].to_string();
+        let descriptor = &descriptor.replace("*", "1");     // e.g. /0/* -> /0/1
+        interface::display_address(&device, &descriptor).unwrap();
     }
 }
