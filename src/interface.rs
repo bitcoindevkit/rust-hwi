87ed0df790d4eb57f13c57ee932930903a99b00e
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2020-03-09 14:45:12 +0100

    Add sign tx, cargo format, add get keypool signature

--- src/interface.rs
+++ src/interface.rs
@@ -1,10 +1,17 @@
 use std::str::from_utf8;
 
+use bitcoin::consensus::encode::serialize;
 use bitcoin::util::bip32::DerivationPath;
+use bitcoin::util::psbt::PartiallySignedTransaction;
+
+use base64;
 
 use crate::commands::{HWICommand, HWIFlag, HWISubcommand};
 use crate::error::Error;
-use crate::types::{HWIAddress, HWIAddressType, HWIDescriptor, HWIDevice, HWIExtendedPubKey, HWISignature};
+use crate::types::{
+    HWIAddress, HWIAddressType, HWIDescriptor, HWIDevice, HWIExtendedPubKey,
+    HWIPartiallySignedTransaction, HWISignature,
+};
 
 pub fn enumerate() -> Result<Vec<HWIDevice>, Error> {
     let output = HWICommand::new()
@@ -21,13 +28,24 @@ pub fn get_master_xpub(device: &HWIDevice) -> Result<HWIExtendedPubKey, Error> {
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
-pub fn sign_tx(device: &HWIDevice) {}
+pub fn sign_tx(
+    device: &HWIDevice,
+    psbt: &PartiallySignedTransaction,
+) -> Result<HWIPartiallySignedTransaction, Error> {
+    let psbt = base64::encode(&serialize(psbt));
+    let output = HWICommand::new()
+        .add_flag(HWIFlag::Fingerprint(device.fingerprint))
+        .add_subcommand(HWISubcommand::SignTx)
+        .add_psbt(&psbt)
+        .execute()?;
+    Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
+}
 
 pub fn get_xpub(device: &HWIDevice, path: &DerivationPath) -> Result<HWIExtendedPubKey, Error> {
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
         .add_subcommand(HWISubcommand::GetXpub)
-        .add_sign_path(path)
+        .add_sign_path(&path)
         .execute()?;
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
@@ -40,18 +58,29 @@ pub fn sign_message(
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
         .add_subcommand(HWISubcommand::SignMessage)
-        .add_message(message)
-        .add_sign_path(path)
+        .add_message(&message)
+        .add_sign_path(&path)
         .execute()?;
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
-pub fn get_keypool(device: &HWIDevice) {}
+pub fn get_keypool(
+    device: &HWIDevice,
+    keypool: bool,
+    internal: bool,
+    address_type: &HWIAddressType,
+    account: Option<u32>,
+    path: &Option<DerivationPath>,
+    start: u32,
+    end: u32,
+) {
+    // Do something
+}
 
 pub fn get_descriptors(device: &HWIDevice, account: Option<u32>) -> Result<HWIDescriptor, Error> {
     let account = match account {
         Some(i) => i,
-        None => 0
+        None => 0,
     };
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
@@ -61,21 +90,28 @@ pub fn get_descriptors(device: &HWIDevice, account: Option<u32>) -> Result<HWIDe
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
-pub fn display_address_with_desc(device: &HWIDevice, descriptor: &String,) -> Result<HWIAddress, Error> {
+pub fn display_address_with_desc(
+    device: &HWIDevice,
+    descriptor: &String,
+) -> Result<HWIAddress, Error> {
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
         .add_subcommand(HWISubcommand::DisplayAddress)
-        .add_descriptor(descriptor)
+        .add_descriptor(&descriptor)
         .execute()?;
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
 
-pub fn display_address_with_path(device: &HWIDevice, path: &DerivationPath, address_type: &HWIAddressType) -> Result<HWIAddress, Error> {
+pub fn display_address_with_path(
+    device: &HWIDevice,
+    path: &DerivationPath,
+    address_type: &HWIAddressType,
+) -> Result<HWIAddress, Error> {
     let output = HWICommand::new()
         .add_flag(HWIFlag::Fingerprint(device.fingerprint))
         .add_subcommand(HWISubcommand::DisplayAddress)
-        .add_key_path(path)
-        .add_address_type(address_type)
+        .add_key_path(&path)
+        .add_address_type(&address_type)
         .execute()?;
     Ok(serde_json::from_str(from_utf8(&output.stdout)?)?)
 }
